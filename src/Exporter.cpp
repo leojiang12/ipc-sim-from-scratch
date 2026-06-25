9e74ea0e36f0951463a129af36b5dd2450ae1d5d
Leo Jiang <leo99jiang@gmail.com>
2026-06-24 17:26:42 -0700

    fixed export bug for mass spring square mesh

--- src/Exporter.cpp
+++ src/Exporter.cpp
@@ -40,38 +40,15 @@ void write_obj(const std::string& output_dir, int frame,
         for (size_t i = 0; i < mesh.ny; i++) {
             for (size_t j = 0; j < mesh.nx; j++) {
 
-                int v0, v1, v2, v3, v4, v5;
-
                 int v00 = id(i, j);
                 int v01 = id(i, j + 1);
                 int v10 = id(i + 1, j);
                 int v11 = id(i + 1, j + 1);
 
-                // If i xor j is even
-                if ((i % 2) ^ (j % 2)) {
-                    v0 = v00;
-                    v1 = v10;
-                    v2 = v01;
-
-                    v3 = v10;
-                    v4 = v11;
-                    v5 = v01;
-                //    mesh.triangles.push_back({v00, v10, v01});  // bl, br, tl
-                //    mesh.triangles.push_back({v10, v11, v01});  // br, tr, tl
-                } else {
-                    v0 = v00;
-                    v1 = v10;
-                    v2 = v11;
-
-                    v3 = v00;
-                    v4 = v11;
-                    v5 = v01;
-                //  mesh.triangles.push_back({v00, v10, v11});  // bl, br, tr
-                //  mesh.triangles.push_back({v00, v11, v01});  // bl, tr, t1
-                }
-
-                f << "f " << v0 + 1 << " " << v1 + 1 << " " << v2 + 1 << "\n";
-                f << "f " << v3 + 1 << " " << v4 + 1 << " " << v5 + 1 << "\n";
+                // bl br tr
+                // bl tr tl
+                f << "f " << v01 + 1 << " " << v10 + 1 << " " << v11 + 1 << "\n";
+                f << "f " << v00 + 1 << " " << v11 + 1 << " " << v01 + 1 << "\n";
             }
         }
 
--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@
 
 int main() {
 
+    // Frame output folder
+    std::string output_dir = "output";
+
     // Square mesh (centered at 0,0)
     int n = 4;
     double length = 1.0;
@@ -18,7 +21,6 @@ int main() {
     State state = build_initial_state(mesh);
     //Eigen::VectorXd initial_velocities = Eigen::VectorXd::Zero(state.x.size());     // v0 = 0 by default on initialization, use for arbitrary velocity
 
-    std::string output_dir = "output";
     int frame = 0;
     write_obj(output_dir, frame, state.x, mesh);
     std::cout << "wrote frame 0 to " << "output_dir" << "/" << frame << ".obj" << std::endl;
